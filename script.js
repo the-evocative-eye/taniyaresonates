28d0be10fc88c455479de22314408909f69cc49f
Taniya <agarwaltaniya49@gmail.com>
2023-10-28 21:45:50 -0700

    third commit

--- script.js
+++ script.js
@@ -7,9 +7,21 @@ $(document).ready(function(){
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
     })
-});
-var btn=document.getElementById('button');
-btn.addEventListener('click',function(e){
-    e.preventDefault()
-    console.log('hi');
-})
+}); 
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticmail.com",
+        Username : "agarwaltaniya49@gmail.com",
+        Password : "8ACD2725D278DC29E5354BB816E0FFD254E7",
+        To : 'agarwaltaniya49@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "Collaboration Details",
+        Body : "Name: "+document.getElementById("name").value 
+        + "<br> Email: "+document.getElementById("email").value
+        + "<br> Phone no.: "+document.getElementById("phone").value
+        +"<br> Message: "+document.getElementById("message").value
+    }).then(
+      message => alert("Message sent successfully")
+    );
+}
